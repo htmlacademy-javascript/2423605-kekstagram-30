8272b25e8989c94880b4a77d954309bba49bb2d3
DYSmirnov <dmitriy_rds@mail.ru>
2023-10-27 12:55:43 +0300

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,22 @@
+function doTime(startTime, endTime, startEventTime, eventTime){
+  const startTimeArray = startTime.split(':').map((time)=>Number(time));
+  const endTimeArray = endTime.split(':').map((time)=>Number(time));
+  const startEventTimeArray = startEventTime.split(':').map((time)=>Number(time));
+  const eventTimeArray = [(Math.floor(eventTime / 60)), (eventTime % 60)];
+  const endEventTimeArray = [Number(startEventTimeArray[0]) + Number(eventTimeArray[0]),
+    Number(startEventTimeArray[1]) + Number(eventTimeArray[1])];
+//проверка, что встреча начнется не раньше, чем начинается рабочий день || заканчивается не позже, чем заканчивается рабочий день
+  if(startTimeArray[0] > startEventTimeArray[0] || endTimeArray[0] < endEventTimeArray[0]){
+    return false;
+  }
+  else if((startTimeArray[0] === startEventTimeArray[0] && startTimeArray[1] > startEventTimeArray[1]) ||
+   (endTimeArray[0] === endEventTimeArray[0] && endTimeArray[1] < endEventTimeArray[1])){
+    return false;
+  }
+  return true;
+}
+
+/*
 //Функция для проверки длины строки.
 function checkLength(string, length){
   return string.length <= length ;
@@ -29,3 +48,4 @@ function getNumber(rawString){
   }
   return parseInt(number);
 }
+*/
